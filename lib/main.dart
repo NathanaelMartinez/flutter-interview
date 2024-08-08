5296b97b1097c91eaa4f0c5381a519e9a65c26d0
NathanaelMartinez <nathanaellmartinez@gmail.com>
2024-08-08 03:07:11 -0600

    Add bottom modal sheet with correct styling - revise styling of text and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() => runApp(const MyApp());
+void main() =>
+    runApp(const MaterialApp(title: 'WeatherForemost', home: MyApp()));
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -13,99 +14,192 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          leading: const WeatherForemostIcon(),
-          title: const Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'WeatherForemost',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    return Scaffold(
+      appBar: AppBar(
+        leading: const WeatherForemostIcon(),
+        title: const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'WeatherForemost',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+        centerTitle: false,
+        backgroundColor: Colors.black,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            WeatherForemostIconNotFound(),
+            Text(
+              'No data Found',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 19,
+                color: Color.fromARGB(255, 94, 93, 93),
+              ),
             ),
-          ),
-          centerTitle: false,
-          backgroundColor: Colors.black,
+            Text(
+              'Please add a city to track its weather',
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 15,
+                  color: Color.fromARGB(255, 94, 93, 93)),
+            ),
+          ],
         ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              WeatherForemostIconNotFound(),
-              Text(
-                'No data Found',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 19,
-                  color: Color.fromARGB(255, 94, 93, 93),
-                ),
-              ),
-              Text(
-                'Please add a city to track its weather',
-                style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15,
-                    color: Color.fromARGB(255, 94, 93, 93)),
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.blue,
-          elevation: 0,
-          child: const Icon(Icons.add, color: Colors.white, size: 50),
-        ),
-        bottomNavigationBar: Container(
-          decoration: const BoxDecoration(
-              border: Border(
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddCityModal(context);
+        },
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        child: const Icon(Icons.add, color: Colors.white, size: 42),
+      ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          border: Border(
             top: BorderSide(
               color: Color.fromARGB(255, 94, 93, 93),
               width: 2.0,
             ),
-          )),
-          child: BottomNavigationBar(
-            elevation: 0,
-            type: BottomNavigationBarType.fixed,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: EdgeInsets.only(left: 50.0),
-                  child: Icon(
-                    Icons.home_outlined,
-                    size: 50,
-                    color: Color.fromARGB(255, 94, 93, 93),
-                  ),
-                ),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.place_outlined,
-                  size: 50,
-                  color: Colors.blue,
-                ),
-                label: 'Location',
-              ),
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: EdgeInsets.only(right: 50.0),
-                  child: Icon(
-                    Icons.person_outline,
-                    size: 50,
-                    color: Color.fromARGB(255, 94, 93, 93),
-                  ),
-                ),
-                label: 'Profile',
-              ),
-            ],
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
           ),
         ),
+        child: BottomNavigationBar(
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.only(left: 90.0),
+                child: Icon(
+                  Icons.home_outlined,
+                  size: 32,
+                  color: Color.fromARGB(255, 94, 93, 93),
+                ),
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.place_outlined,
+                size: 32,
+                color: Colors.blue,
+              ),
+              label: 'Location',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.only(right: 90.0),
+                child: Icon(
+                  Icons.person_outline,
+                  size: 32,
+                  color: Color.fromARGB(255, 94, 93, 93),
+                ),
+              ),
+              label: 'Profile',
+            ),
+          ],
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+        ),
       ),
+    );
+  }
+
+  void _showAddCityModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return DraggableScrollableSheet(
+          expand: false,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return SingleChildScrollView(
+              controller: scrollController,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 16.0),
+                    Container(
+                      width: 32,
+                      height: 4,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 121, 116, 126),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    const SizedBox(height: 56.0),
+                    const TextField(
+                      decoration: InputDecoration(
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        label: Text(
+                          'Add City',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 73, 69, 79),
+                          ),
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const TextField(
+                      maxLines: 4,
+                      decoration: InputDecoration(
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        label: Text(
+                          'Description',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 73, 69, 79),
+                          ),
+                        ),
+                        hintText: 'Add a description',
+                        hintStyle: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                          color: Color.fromARGB(255, 153, 153, 153),
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 79),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: const MaterialStatePropertyAll(
+                              Color.fromARGB(255, 153, 153, 153),
+                            ),
+                            shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                            )),
+                        onPressed: () {
+                          // Handle save action
+                        },
+                        child: const Text(
+                          'Save City',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
@@ -115,7 +209,6 @@ class WeatherForemostIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Image.asset("images/logo.png"); //TODO: create custom Icon
     return const Icon(Icons.cloud);
   }
 }
@@ -125,8 +218,6 @@ class WeatherForemostIconNotFound extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return const ImageIcon(AssetImage(
-    //     "icons/logoSearch.png")); //TODO: create custom not Found Icon
     return const Icon(Icons.cloud);
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,19 +4,25 @@ import 'package:interview_flutter/main.dart';
 
 void main() {
   testWidgets('App builds without errors', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
     expect(find.text('No data Found'), findsOneWidget);
   });
 
   testWidgets('Leading icon is present in the AppBar',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
     expect(find.byType(WeatherForemostIcon),
         findsOneWidget); // Update with your custom icon finder
   });
 
   testWidgets('AppBar title is styled correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
     final appBarTitle = find.text('WeatherForemost');
     expect(appBarTitle, findsOneWidget);
     final appBar = tester.widget<AppBar>(find.byType(AppBar));
@@ -30,7 +36,9 @@ void main() {
 
   testWidgets('Body displays correct initial content',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
     expect(find.byType(WeatherForemostIconNotFound), findsOneWidget);
 
     final noDataText = find.text('No data Found');
@@ -48,7 +56,9 @@ void main() {
 
   testWidgets('Floating action button is displayed and styled correctly',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
     final fab = find.byType(FloatingActionButton);
 
     final FloatingActionButton fabWidget =
@@ -58,13 +68,15 @@ void main() {
     final icon = fabWidget.child as Icon;
     expect(icon.icon, Icons.add);
     expect(icon.color, Colors.white);
-    expect(icon.size, 50.0);
+    expect(icon.size, 42.0);
     expect(fabWidget.elevation, 0.0);
   });
 
   testWidgets('Add styled BottomNavigationBar with 3 styled icons',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
 
     // check the BottomNavigationBar is present
     final bottomNavBar = find.byType(BottomNavigationBar);
@@ -77,26 +89,28 @@ void main() {
     final firstItemIcon = widget.items[0].icon as Padding;
     final firstIcon = firstItemIcon.child as Icon;
     expect(firstIcon.icon, Icons.home_outlined);
-    expect(firstIcon.size, 50);
+    expect(firstIcon.size, 32);
     expect(firstIcon.color, const Color.fromARGB(255, 94, 93, 93));
 
     // check the second item
     final secondIcon = widget.items[1].icon as Icon;
     expect(secondIcon.icon, Icons.place_outlined);
-    expect(secondIcon.size, 50);
+    expect(secondIcon.size, 32);
     expect(secondIcon.color, Colors.blue);
 
     // check the third item
     final thirdItemIcon = widget.items[2].icon as Padding;
     final thirdIcon = thirdItemIcon.child as Icon;
     expect(thirdIcon.icon, Icons.person_outline);
-    expect(thirdIcon.size, 50);
+    expect(thirdIcon.size, 32);
     expect(thirdIcon.color, const Color.fromARGB(255, 94, 93, 93));
   });
 
   testWidgets('Custom border on BottomNavigationBar',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
 
     // check that Container of BottomNavigationBar exists
     final container = find.byType(Container);
@@ -115,5 +129,73 @@ void main() {
       }
     }
     expect(found, isTrue);
+  });
+
+  testWidgets('FloatingActionButton opens modal bottom sheet with form',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
+
+    // verify FAB exists and tap it
+    final fab = find.byType(FloatingActionButton);
+    expect(fab, findsOneWidget);
+    await tester.tap(fab);
+    await tester.pumpAndSettle();
+    // wait for modal bottom sheet to appear
+
+    // verify modal bottom sheet contains form
+    expect(find.text('Add City'), findsOneWidget);
+    expect(find.text('Description'), findsOneWidget);
+    expect(find.text('Add a description'), findsOneWidget);
+    expect(find.text('Save City'), findsOneWidget);
+  });
+  testWidgets('Modal bottom sheet form elements have correct properties',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: MyApp(),
+    ));
+
+    /// verify FAB exists and tap it
+    final fab = find.byType(FloatingActionButton);
+    await tester.tap(fab);
+    await tester.pumpAndSettle();
+    // wait for modal bottom sheet to appear
+
+    // check properties of 'Add City' input
+    final addCityField = find.widgetWithText(TextField, 'Add City');
+    expect(addCityField, findsOneWidget);
+
+    final addCityFieldWidget = tester.widget<TextField>(addCityField);
+    final addCityLabel = addCityFieldWidget.decoration?.label as Text?;
+    expect(addCityLabel?.data, 'Add City');
+    expect(addCityFieldWidget.decoration?.border,
+        isInstanceOf<OutlineInputBorder>());
+
+    // check properties of 'Description' input
+    final descriptionField = find.widgetWithText(TextField, 'Description');
+    expect(descriptionField, findsOneWidget);
+
+    final descriptionFieldWidget = tester.widget<TextField>(descriptionField);
+    final descriptionLabel = descriptionFieldWidget.decoration?.label as Text?;
+    expect(descriptionLabel?.data, 'Description');
+    expect(descriptionFieldWidget.decoration?.hintText, 'Add a description');
+    expect(descriptionFieldWidget.decoration?.hintStyle?.color,
+        const Color.fromARGB(255, 153, 153, 153));
+    expect(descriptionFieldWidget.decoration?.hintStyle?.fontSize, 24);
+    expect(descriptionFieldWidget.decoration?.border,
+        isInstanceOf<OutlineInputBorder>());
+    expect(descriptionFieldWidget.maxLines, 4);
+
+    // check properties of 'Save City' button
+    final saveButton = find.widgetWithText(ElevatedButton, 'Save City');
+    expect(saveButton, findsOneWidget);
+
+    final saveButtonWidget = tester.widget<ElevatedButton>(saveButton);
+    expect(saveButtonWidget.style?.backgroundColor?.resolve({}),
+        const Color.fromARGB(255, 153, 153, 153));
+    final shape =
+        saveButtonWidget.style?.shape?.resolve({}) as RoundedRectangleBorder?;
+    expect(shape?.borderRadius, BorderRadius.circular(4));
   });
 }
